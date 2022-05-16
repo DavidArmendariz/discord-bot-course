8e78880d0eb6788fadc90cb1d98548b9127200b9
David Armendariz <darmendariz1998@outlook.com>
2022-05-15 20:05:08 -0500

    Removed permissions

--- src/listeners/on-ready.ts
+++ src/listeners/on-ready.ts
@@ -1,38 +1,11 @@
 import { Client } from 'discord.js';
-import { PERMISSIONS } from '../permissions';
 import { SlashCommands } from '../slash-commands';
 
 export const onReady = (client: Client) => {
   client.on('ready', async () => {
-    const setPermissions = async () => {
-      for (const permission of PERMISSIONS) {
-        const { guildId, permissions } = permission;
-        const globalCommand = applicationCommands?.find(
-          (command) => command.name === permission.commandName
-        );
-        await globalCommand?.permissions.set({
-          guild: guildId,
-          permissions,
-        });
-
-        const guild =
-          client.guilds.cache.get(guildId) ||
-          (await client.guilds.fetch(guildId));
-        const guildCommands = await guild.commands.fetch();
-        const guildCommand = guildCommands.find(
-          (command) => command.name === permission.commandName
-        );
-        guildCommand?.permissions.set({ permissions });
-      }
-    };
-
     console.log(`${client.user?.username} is online`);
 
     const commands = SlashCommands.map(({ command }) => command.toJSON());
-    const applicationCommands = await client.application?.commands.set(
-      commands
-    );
-
-    await setPermissions();
+    await client.application?.commands.set(commands);
   });
 };
